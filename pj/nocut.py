97c0019e2bec79ebae53339444ee0d92f478dea1
NoPyeongJin <opener517@naver.com>
2018-11-24 16:47:55 +0900

    modify error

--- pj/nocut.py
+++ pj/nocut.py
@@ -34,22 +34,30 @@ def crawling_nocut(page, json_path):
     dataAll = OrderedDict()
     data_idx = 0
 
+    #pnlNewsList > ul > li:nth-of-type(1) > dl > dt > a
     for i in range(1, page+1):
         
-        url = url + str(i) # URL + page
-        request = requests.get(url)
+        params = {
+            'page' : str(i)
+        }
+        request = requests.get(url, params=params)
         html = request.content
         soup = BeautifulSoup(html, 'html5lib')
 
-        news_list = soup.select('#pnlNewsList > ul.newslist > li > dl')
+        news_list = soup.select('#pnlNewsList > ul > li > dl')
         
         for news in news_list:
+            
+            try:
+                _1 = news.select('dt > a')[0] # title & link
+                _2 = news.select('dd.txt > a')[0] # description, date
+            except Exception as e:
+                print(news, i, data_idx)
+                print(e)
 
-            _1 = news.select('dt > a')[0] # title & link
             news_title = _1.text 
             news_link = headURL + _1['href'] + tailURL + str(i)
             
-            _2 = news.select('dd.txt > a')[0] # description, date
             news_date = _2.text[-21:]
 
             # make dict obj
